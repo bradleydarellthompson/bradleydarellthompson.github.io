aa2435eaa85222e71296130a4ed923679a87833f
bdthompson622 <bdthompson@ualr.edu>
2025-01-31 01:53:32 -0600

    inverse
    Added inverse <p> color selector

--- myscript.js
+++ myscript.js
@@ -2,6 +2,16 @@ function handleAlignmentChange() {
     var selectedAlign = document.getElementById("alignment").value;
     const h1Element = document.querySelector('h1');
     h1Element.style.textAlign = selectedAlign;
-  }
+}
 
-  document.getElementById("alignment").addEventListener("change", handleAlignmentChange);
+document.getElementById("alignment").addEventListener("change", handleAlignmentChange);
+
+function handlePColorChange() {
+    var selectedColor = document.getElementById("paragraphcolor").value;
+    const pElements = document.querySelectorAll('p');
+    pElements.forEach(pElement => {
+        pElement.className = selectedColor;
+    });
+}
+
+document.getElementById("paragraphcolor").addEventListener("change", handlePColorChange);
